1f10bd4dc3d679068b3b587517124ee9d6ebebd9
Chris_Barnett <barney.chrisj@gmail.com>
2022-11-06 13:11:00 +0000

    delete stuff

--- lib/providers/firestore/firebase_firestore_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final firebaseFirestoreProvider = Provider((ref) {
-  final firebaseFirestoreInstance = FirebaseFirestore.instance;
-  return firebaseFirestoreInstance;
-});
--- lib/providers/socket/socket_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:decisionroll/services/socket_service.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final socketServiceProvider = Provider<SocketService>((ref) {
-  final socketService = SocketService();
-  socketService.initConnection();
-  return socketService;
-});
