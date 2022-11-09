8f2e74a9ebfed5510916fdc8299df0cbe15234ba
Heroes_Project <heroesprojecttz@gmail.com>
2022-11-09 13:10:13 +0000

    create VoteModel and myVote query

--- lib/models/database/candidate_model.dart
+++ lib/models/database/candidate_model.dart
@@ -1,26 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CandidateModel {
-  final DocumentSnapshot? snapshot;
-  final String? id;
   final String title;
   final int weight;
+  final int colorIdx;
 
   const CandidateModel(
-      {this.snapshot, this.id, required this.title, this.weight = 0});
+      {required this.title, this.weight = 0, this.colorIdx = 0});
 
   factory CandidateModel.fromSnapshot(
       DocumentSnapshot<Map<String, dynamic>> snapshot) {
     final map = snapshot.data() ?? {};
-
-    return CandidateModel(
-        snapshot: snapshot,
-        id: snapshot.id,
-        title: map['title'] ?? '<missing title>',
-        weight: map['weight'] ?? 0);
+    return CandidateModel.fromMap(map);
   }
+
+  factory CandidateModel.fromMap(Map<String, dynamic> map) => CandidateModel(
+      title: map['title'] ?? '<missing title>',
+      weight: map['weight'],
+      colorIdx: map['colorIdx']);
+
   Map<String, dynamic> toMap() {
-    return {'title': title, 'weight': weight};
+    return {'title': title, 'weight': weight, 'colorIdx': colorIdx};
   }
 
   String toJson() {
