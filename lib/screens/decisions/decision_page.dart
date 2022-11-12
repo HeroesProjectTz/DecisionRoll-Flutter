67b6fadb48a2ceedac61f92a8fe9e579e4be6f5b
Heroes_Project <heroesprojecttz@gmail.com>
2022-11-12 01:03:16 +0000

    wrap pages in check_auth

--- lib/screens/decisions/decision_page.dart
+++ lib/screens/decisions/decision_page.dart
@@ -1,32 +1,15 @@
-import 'package:decisionroll/common/routes.dart';
-import 'package:decisionroll/screens/decisions/candidate_vote_control.dart';
 import 'package:decisionroll/screens/decisions/state_transition_button_widget.dart';
 import 'package:decisionroll/screens/decisions/status_text_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:decisionroll/common/my_appbar.dart';
 import 'package:decisionroll/common/my_drawer.dart';
 import 'package:decisionroll/common/sizeConfig.dart';
-import 'package:decisionroll/utilities/colors.dart';
-import 'package:qr_flutter/qr_flutter.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:decisionroll/providers/database/decision_candidates_provider.dart';
-import 'package:decisionroll/common/bubble_loading_widget.dart';
-import 'package:decisionroll/models/database/candidate_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-import '../../common/candidate_colors.dart';
-import '../../models/database/decision_model.dart';
-import '../../providers/database/database_provider.dart';
-import '../../providers/database/decision_account_provider.dart';
-import '../../providers/database/decision_candidate_controls_provider.dart';
-import '../../providers/database/decision_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../decisions/candidate_add_widget.dart';
 import 'candidate_vote_controls_widget.dart';
 import 'candidates_wheel_widget.dart';
 import 'decision_app_bar_widget.dart';
 
-class DecisionPage extends StatelessWidget {
+class DecisionPage extends ConsumerWidget {
   final String decisionId;
 
   const DecisionPage({
@@ -35,7 +18,7 @@ class DecisionPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext c) {
+  Widget build(BuildContext c, WidgetRef ref) {
     return Scaffold(
         // backgroundColor: purpleColor,
         appBar: DecisionAppBar(decisionId),
